f80f90d227d38f605f7e3a4367bad4d9c4928278
Paul Klene <pklene@radford.edu>
2018-10-06 16:28:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,37 @@
 <html>
 <head>
 <title>VIMS</title>
-<link rel="stylesheet" type="text/css" href="login.css">
+<style>
+button {
+    background-color: #E6E6E6;
+    color: black;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 5%;
+	font-weight: bold;
+	text-align: center;
+}
+
+
+input[type=text], input[type=password] {
+	background-color: white;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+.login{
+    border: 3px solid;
+	border-color: black;
+	padding:10px;
+	width: 350px;
+	margin: auto;
+}
+</style>
 </head>
 <body>
 <h1><center>Valor Inventory Management System</center></h1>
